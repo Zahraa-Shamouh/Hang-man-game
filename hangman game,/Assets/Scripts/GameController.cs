e25d87e304b99b307fecedb743c005b95fcf6004
Zahraa-shamouh <zahraa.m.shamouh@gmail.com>
2021-01-04 00:28:51 +0300

    int commit
    should  figure out the error in GameController.cs line 26.

---
+++ hangman game,/Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; //so we can get the text. 
+
+public class GameController : MonoBehaviour
+{
+    public Text wordIndicator;    //how many words 
+    public Text scoreIndicator;   // to follow up with the scoure
+    public Text letterIndicator;  // indicator for the unmbers of letters
+
+
+    private HangmanController hangman;  //hangmane reference 
+
+    private string word;         //[word] which will be stord as string
+    private char[] Revealed;     //for each time the word is set.
+    private int score;
+    private bool GameCompleted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //initilze 
+       // hangman = GameObject.FindGameObjectsWithTag("player").GetComponent<HangmanController>(); 
+        reset(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void next() { 
+    }
+
+    void reset()
+    {
+        //setting the scoure 
+        score = 0;                //the initial value of the score!
+        GameCompleted = false;    //game is not finish. 
+        next();                   //call fuction in a function. 
+    }
+}
---
+++ hangman game,/Assets/Scripts/HangmanController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangmanController : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject body;
+    public GameObject arms;
+    public GameObject legs;
+
+
+    private int tries;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] parts = new GameObject[] { legs, arms, body, head };
+        tries = parts.Length - 1; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
